f62a622701d5a21235731e6073f7dc86c4b7c919
Ricardo <lrsj2003@gmail.com>
2023-03-17 17:22:59 -0300

    botao de feito agora muda de aparencia

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -3,12 +3,21 @@ import styles from "../styles/TaskItem.module.css";
 export default function TaskItem({ todo, doneTodoHandler, editTodo, deleteTodo }) {
     return (
         <div className={styles.task_item_box}>
+
+            {todo.isDone ?
             <button
                 className={styles.done_button}
                 onClick={() => doneTodoHandler(todo.id)}>
                 Feito
             </button>
-
+                :
+            <button
+                className={styles.not_done_button}
+                onClick={() => doneTodoHandler(todo.id)}>
+                Feito
+            </button>
+            }
+            
             {todo.isDone ?
                 <p className={styles.done_text}>{todo.text}</p>
                 :
