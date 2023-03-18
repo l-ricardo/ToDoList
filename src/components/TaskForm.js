9bc26133eb79ffa4582ec8fe9584e78be331a6fc
Ricardo <lrsj2003@gmail.com>
2023-03-18 00:59:01 -0300

    validando o input

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -22,7 +22,9 @@ export default function TaskForm({ addTask, inputText }) {
     }
 
     const submitHandler = () => {
-        newTask(text)
+        if (text.trim() !== "") {
+            newTask(text)
+        }
         setText("")
     }
 
