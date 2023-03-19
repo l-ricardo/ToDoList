5803d333c947d01ce94dbdda4fa77421d4b3f045
Ricardo <lrsj2003@gmail.com>
2023-03-18 22:55:39 -0300

    indentacao

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import "react-toastify/dist/ReactToastify.css"
 import "./styles/App.css"
 
 export default function App() {
-  return (
-    <>
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
+    return (
+        <>
+            <ToastContainer
+                position="top-center"
+                autoClose={2000}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
 
-      <h1>Minhas tarefas</h1>
+            <h1>Minhas tarefas</h1>
 
-      <TaskList />
-    </>
-  )
+            <TaskList />
+        </>
+    )
 }
